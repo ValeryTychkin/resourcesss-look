f0c40b0b44acf9b3a169619dad494633a3247c07
ValeryTychkin <valery.tychkin@gmail.com>
2023-08-12 15:49:07 +0400

    Upd routes for tests

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,8 +1,25 @@
 import Fluent
 import Vapor
 
+struct RamInfoResponse: Content {
+    var total: Int = 0
+    var usage: Int = 0
+    var availble: Int = 0
+    var usagePercent: Float = 0
+}
+
 func routes(_ app: Application) throws {
     app.get("health-check") { req async in
-        "OK"
+        return "OK"
+    }
+    
+    app.get("ram-info") { req async -> RamInfoResponse in
+        let ramInfo = RamInfoResponse()
+        return ramInfo
+    }
+    
+    app.get("test") { req async in
+        let result = SocketClient.init()
+        return result.cRunFunction()
     }
 }
